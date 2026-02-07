71028d22964f7c01c85915c9555e11933cd81fb6
bavuchoko <bavuchoko@naver.com>
2026-02-07 12:38:02 +0900

    [update] 임대인 vo 수정함

--- system-realtor/src/main/kotlin/com/system/realtor/domain/property/Landlord.kt
+++ system-realtor/src/main/kotlin/com/system/realtor/domain/property/Landlord.kt
@@ -2,6 +2,9 @@ package com.system.realtor.domain.property
 
 data class Landlord(
     val name:String,
-    val contact: String,
+    val mobile: String,
+    val phone: String,
+    val address: Address,
+    val registryNumber:String,
     val description: String
 )
