4a21ef9be84bc1becaafeac45a059abf5d54ff76
bavuchoko <bavuchoko@naver.com>
2026-02-07 11:25:52 +0900

    [update] 연관관계 추가

--- system-realtor/src/main/kotlin/com/system/realtor/domain/property/Property.kt
+++ system-realtor/src/main/kotlin/com/system/realtor/domain/property/Property.kt
@@ -1,12 +1,15 @@
 package com.system.realtor.domain.property
 
+import com.system.realtor.domain.Realtor
+
 /**
 매물
  */
 class Property(
    val building: String,
    val unit: String,
-   val propertyType: PropertyType
+   val propertyType: PropertyType,
+   val realtor: Realtor
 ) {
 
 }
